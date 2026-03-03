fb679a324438b476424c7789ceae86904a364b20
Lucas Weidmann <weidmlucas@gmail.com>
2026-03-02 22:58:42 -0800

    adding goddess tear

---
+++ Content/Items/Materials/GoddessTear.cs
@@ -0,0 +1,24 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+using Terraria.GameContent.Creative;
+
+namespace Lunar.Content.Items.Materials
+{
+    public class GoddessTear : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 18;
+            Item.height = 26;
+            Item.maxStack = 9999;
+            Item.value = 400;
+            Item.rare = ItemRarityID.Blue;
+        }
+    }
+}
